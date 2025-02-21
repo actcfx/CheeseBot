218555bd8bf4b115834b87e96f388be2efc40e97
北極狐 <cheung950624@gmail.com>
2025-02-21 09:56:05 +0800

    Style: Remove unused import.

--- src/commands/test/errorTest.js
+++ src/commands/test/errorTest.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const { developerId } = require('../../../config/config.json');
 
 module.exports = {
