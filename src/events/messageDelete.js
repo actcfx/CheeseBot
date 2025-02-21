1ba759183e332361629e817fd16522480221a4cf
北極狐 <cheung950624@gmail.com>
2025-02-21 14:28:42 +0800

    Docs: Add comments for clarity in messageDelete.js event handling

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -6,13 +6,15 @@ const { deleteAttachmentChannelId } = require('../../config/discord.json');
 
 module.exports = {
     name: Events.MessageDelete,
-    once: false,
+    once: false, // Indicates that this event will run multiple times
     execute(message) {
         if (message.guildId !== guildId) return;
         if (message.author.bot) return;
 
         const deleteMessageChannel = message.guild.channels.cache.get(deleteMessageChannelId);
         const deleteAttachmentChannel = message.guild.channels.cache.get(deleteAttachmentChannelId);
+
+        // Create an embed message for the deleted message
         const deleteMessageEmbed = new EmbedBuilder()
             .setColor("#FC9A66")
             .setTitle("訊息刪除")
@@ -22,24 +24,28 @@ module.exports = {
             )
             .setTimestamp();
 
+        // Add the content of the deleted message to the embed if it exists
         if (message.content) {
             deleteMessageEmbed.addFields(
                 { name: "內容", value: message.content, inline: false }
             );
         }
 
+        // If there are attachments, add them to the embed and send them to the attachment channel
         if (message.attachments.size > 0) {
             for (const attachment of message.attachments.values()) {
                 try {
                     deleteMessageEmbed.addFields(
-                        {name: "附件", value: attachment.url, inline: false}
-                    )
+                        { name: "附件", value: attachment.url, inline: false }
+                    );
                     deleteAttachmentChannel.send({ content: attachment.url });
                 } catch { }
             }
         }
 
         console.log(`[EVENT]    | A message by ${message.author.tag} was deleted.`);
+
+        // Send the embed message to the delete message channel and log the message link
         deleteMessageChannel.send({ embeds: [deleteMessageEmbed] }).then(sentMessage => {
             const deleteMessageLink = sentMessage.url;
             console.log(`└─ [LINK]  | ${deleteMessageLink}`);
