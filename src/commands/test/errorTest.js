3a1e3c345c8e271c00f7c216b30139c433a669a4
北極狐 <cheung950624@gmail.com>
2025-02-21 14:30:11 +0800

    Docs: Add comments for clarity in errorTest.js command execution

--- src/commands/test/errorTest.js
+++ src/commands/test/errorTest.js
@@ -17,19 +17,25 @@ module.exports = {
                 )),
     async execute(interaction) {
         console.log(`[/COMMAND] | <${interaction.user.tag}> executed the error_test command.`);
+
+        // Check if the user is the developer
         if (interaction.user.id !== developerId) {
             interaction.reply({ content: 'This command is only for the developer!', ephemeral: true });
             console.log('└─ [REPLY] | This command is only for the developer!');
             return;
         }
 
+        // Get the type of error to test
         const errorType = interaction.options.getString('type');
 
+        // Trigger the specified error type
         switch (errorType) {
             case 'reference':
+                // Trigger a ReferenceError by using an undefined variable
                 interaction.reply(undefinedVariable);
                 break;
             case 'type':
+                // Trigger a TypeError by accessing a property of null
                 interaction.reply(null.property);
                 break;
         }
